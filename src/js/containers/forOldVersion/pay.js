8f920964a2d832fda7c7187bb272c7910a1163e5
walljack <walljack@163.com>
2017-09-06 18:51:34 +0800

    修改pay页面警告提示

--- src/js/containers/forOldVersion/pay.js
+++ src/js/containers/forOldVersion/pay.js
@@ -2,16 +2,18 @@ import React from "react";
 import {connect} from "react-redux";
 import {withRouter} from "react-router-dom";
 import BaseComponent from "../../components/common/BaseComponent";
-import {getQueryString, linkTo} from "../../utils/comUtils";
 
 class Pay extends BaseComponent {
     constructor(props) {
         super(props);
-        const bondDeviceId = getQueryString("bondDeviceId");
+    }
+
+    componentDidMount() {
         const {state, pollingId, deviceId, openid} = this.props.match.params;
         const link = `pay?state=${state || ""}&pollingId=${pollingId || ""}&deviceId=${deviceId || ""}&openid=${openid || ""}`;
         this.props.history.replace("/" + link);
     }
+
     render() {
         return (
             <div>
