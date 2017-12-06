31e73ce09f34f7f997ab88e518adb807eb51232b
zed <2473633284@qq.com>
2017-12-06 15:51:42 +0800

    f4配置修复

--- src/config/webpack.dev.f4.js
+++ src/config/webpack.dev.f4.js
@@ -21,7 +21,7 @@ module.exports = Merge(CommonConfig, {
     plugins: [
         new webpack.DefinePlugin({
             'process.env': {
-                'NODE_ENV': JSON.stringify('development')
+                'NODE_ENV': JSON.stringify('f4')
             }
         })
     ]
