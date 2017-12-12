c37179752f92d192162ba5a4862e27ae45ed0f40
walljack <walljack@163.com>
2017-12-12 17:32:18 +0800

    修改商城链接到正式链接

--- src/config/project.config.dev.js
+++ src/config/project.config.dev.js
@@ -8,7 +8,9 @@ module.exports = {
     environment: 'test',
     apiDomain: 'http://portaltest.j-make.com.cn',
     contextPath : "/",
-    mallIndex: 'https://jmake.haitai.tv/haitai-weisite/shop/index',
-    mallOrder: 'https://jmake.haitai.tv/haitai-weisite/shop/order_list',
+    // mallIndex: 'https://jmake.haitai.tv/haitai-weisite/shop/index',
+    // mallOrder: 'https://jmake.haitai.tv/haitai-weisite/shop/order_list',
+    mallIndex: 'https://jmake.huo.so/shop/index',
+    mallOrder: 'https://jmake.huo.so/shop/order_list',
     appId: "wx4688d2b7b85451aa"
 };
