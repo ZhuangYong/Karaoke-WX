bf5a7da9083765f828fab87e53b613101cd802ed
walljack <walljack@163.com>
2017-12-20 14:59:19 +0800

    录音点击穿透

--- src/js/containers/play/audio.js
+++ src/js/containers/play/audio.js
@@ -98,7 +98,7 @@ class PlayAudio extends BaseComponent {
             <div className="audio-play">
                 <div className="top-panel" style={topPanelStyle}>
                     <AutoPlaySwipeAbleViews className="swipe-panel" style={swipePanelStyle}>
-                        <div className="img-div" onClick={f => location.href = sysConfig.mallIndex}><img src={SlidePngMall1}/></div>
+                        <div className="img-div" onTouchTap={f => location.href = sysConfig.mallIndex}><img src={SlidePngMall1}/></div>
                         <div className="img-div"><img src={SlidePng1}/></div>
                         <div className="img-div"><img src={SlidePng2}/></div>
                         <div className="img-div"><img src={SlidePng3}/></div>
