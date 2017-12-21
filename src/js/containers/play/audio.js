692b3abc0fbadd644eb9e3a3a61f76372582e8a7
walljack <walljack@163.com>
2017-12-21 19:19:29 +0800

    测试录音

--- src/js/containers/play/audio.js
+++ src/js/containers/play/audio.js
@@ -104,10 +104,10 @@ class PlayAudio extends BaseComponent {
             <div className="audio-play">
                 <div className="top-panel" style={topPanelStyle}>
                     <div>
-                        <AutoPlaySwipeAbleViews className="swipe-panel" style={swipePanelStyle}>
-                           {/* <div className="img-div" onTouchTap={f => location.href = sysConfig.mallIndex}><img src={SlidePngMall1}/></div>
+                        <AutoPlaySwipeAbleViews className="swipe-panel" style={{overflow: 'hidden', ...swipePanelStyle}}>
+                            <div className="img-div" onTouchTap={f => location.href = sysConfig.mallIndex}><img src={SlidePngMall1}/></div>
                             <div className="img-div"><img src={SlidePng1}/></div>
-                            <div className="img-div"><img src={SlidePng2}/></div>*/}
+                            <div className="img-div"><img src={SlidePng2}/></div>
                             <div className="img-div"><img src={SlidePng3}/></div>
                         </AutoPlaySwipeAbleViews>
                     </div>
